bd7fc10ddf57d0477eea6906e63a77ec7ecca516
Pritam Dey <pritamdey2001@gmail.com>
2023-02-27 18:30:31 +0530

    testing

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
 	mongoose.set('strictQuery', true);
 	const connection = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log(`MongoDB connected: ${connection.connection.host}`.cyan.bold)
+	console.log(`MongoDB connected: ${connection.connection.host}`);
 };
 
 module.exports = connectDB; // We can use this code from anywhere by importing the exported function
--- server.js
+++ server.js
@@ -12,7 +12,6 @@
 
 /* ========= Server using ExpressJs ========= */
 const express = require('express');
-const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db'); // Importing the db
@@ -55,4 +54,4 @@ app.use('/api/todo/auth', require('./routes/user')); // Can be accessed using ht
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`.red.underline.bold)); // .red.underline.bold -> colors package | Server listening on port 3000
+app.listen(PORT, console.log(`Server running on port ${PORT}`)); // .red.underline.bold -> colors package | Server listening on port 3000
